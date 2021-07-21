13cb03e0361c59096e2c99e739db4242f6b94682
gpatruno <gaetan.patruno@ynov.com>
2021-07-21 13:52:54 +0200

    add logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent {
   }
 
   ngAfterViewInit() {
-    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+    this.observer.observe(['(max-width: 900px)']).subscribe((res) => {
       if (res.matches) {
         this.sidenav.mode = 'over';
         this.sidenav.close();
