a03f4d3ec0eadaf857e6408b9b19e197fd9cb225
gpatruno <gaetan.patruno@ynov.com>
2022-11-01 01:03:28 +0100

    update source

--- src/app/competence/competence.component.spec.ts
+++ src/app/competence/competence.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CompetenceComponent } from './competence.component';
 
@@ -6,7 +6,7 @@ describe('CompetenceComponent', () => {
   let component: CompetenceComponent;
   let fixture: ComponentFixture<CompetenceComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CompetenceComponent ]
     })
--- src/app/formation/formation.component.spec.ts
+++ src/app/formation/formation.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FormationComponent } from './formation.component';
 
@@ -6,7 +6,7 @@ describe('FormationComponent', () => {
   let component: FormationComponent;
   let fixture: ComponentFixture<FormationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FormationComponent ]
     })
